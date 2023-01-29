2b764a1ff8371f0859370a829ecf9fa1f5425ed5
Kiran6785 <kiranjeet.kaur@wittybrains.com>
2023-01-29 13:28:46 +0530

    commit message

---
+++ client/src/components/aboutpagecontent.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+import Zoom from 'react-reveal/Zoom';
+
+
+
+class AboutPageContent extends Component {
+  render() {
+    return (
+
+        <Zoom delay={400}>
+ <div className="about-area">
+        <div className="container p-0">
+            <div className="row m-0">
+                
+    
+                <div className="col-lg-12 col-md-12 p-0">
+                   
+                <div className="about">
+        <div className="container">
+            <div className="row">
+                
+
+                <div className="col-md-12">
+                  
+                    <h2>ABOUT US</h2>
+                    <div className="about-content">
+                    A people can focus on their work and their time by Using P2P delivery and can book delivery guy.                       </div>
+
+                    </div>
+         
+               
+
+                
+            </div>
+        </div>
+    </div>
+
+
+   
+    <div className="col-md-12">
+
+    <br/><br/>
+
+    <div className="im">
+
+   <Zoom delay={500}>
+   <h2>Why Choose Us</h2>
+                        <br/><br/>
+                        <ul className="im-list">
+                                <li className="font-li">This is a online platform where you can easily book and become a delivery partner</li>
+								<br/>
+                                <li className="font-li"> We are committed to high quality delivery partner across the country.</li>
+								
+								<br/>
+                               <li className="font-li"> It gives a sense of assurance so that the person can travel freely without any worries</li>
+							   <br/>
+								 
+						
+                        <li className="font-li">We understand the industry research needs thoroughly and before taking up orders having any Technical/regulatory concerns with respect to Process, Quality,Potency,Safet etc., from clients, our technical team is ready to interact and discuss with the technical team at client’s end to better understand the costumer concern/requirement for best outcome and timely delivery of quality products.  </li>
+							<br/>
+							<li className="font-li">	Paper work, proof of ownership, insurance, accessible and inaccessible quarters, final payment is collected by the delivery partner which is based on size and weight of parcel to drop.</li> 
+							<br/><br/>
+						<li className="font-li">.Just fill in our short form, tell us your parcel requirements and we will match a delivery type(s) to exactly fit your needs. <br/>
+							<br/>It’s that simple!</li>
+							
+							
+							
+                            </ul></Zoom> 
+    </div>
+                       
+
+    </div>
+
+
+                      
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+        </Zoom>
+    );   
+  }
+}
+
+export default AboutPageContent;
